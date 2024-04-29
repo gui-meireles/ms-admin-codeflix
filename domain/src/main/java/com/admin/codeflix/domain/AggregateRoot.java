5e005cde51567f95e4293c7a45ac8c0bf913bac2
gui-meireles <guilhermemeirelesnascimento@gmail.com>
2024-04-28 21:24:10 -0300

    Creating Identifier class

---
+++ domain/src/main/java/com/admin/codeflix/domain/AggregateRoot.java
@@ -0,0 +1,8 @@
+package com.admin.codeflix.domain;
+
+public class AggregateRoot<ID extends Identifier> extends Entity<ID> {
+
+    public AggregateRoot(final ID id) {
+        super(id);
+    }
+}
---
+++ domain/src/main/java/com/admin/codeflix/domain/Identifier.java
@@ -0,0 +1,4 @@
+package com.admin.codeflix.domain;
+
+public abstract class Identifier extends ValueObject {
+}
