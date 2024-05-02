ac682ad2ee48d0d5c46dc30beadc67f3606606f4
gui-meireles <guilhermemeirelesnascimento@gmail.com>
2024-05-02 19:44:08 -0300

    Adding Either and Notification Pattern in Use Case

--- application/src/main/java/com/admin/codeflix/application/category/create/DefaultCreateCategoryUseCase.java
+++ application/src/main/java/com/admin/codeflix/application/category/create/DefaultCreateCategoryUseCase.java
@@ -3,6 +3,7 @@ package com.admin.codeflix.application.category.create;
 import com.admin.codeflix.domain.category.Category;
 import com.admin.codeflix.domain.category.CategoryGateway;
 import com.admin.codeflix.domain.validation.handler.Notification;
+import io.vavr.API;
 import io.vavr.control.Either;
 
 import java.util.Objects;
@@ -25,10 +26,13 @@ public class DefaultCreateCategoryUseCase extends CreateCategoryUseCase {
         final var aCategory = Category.newCategory(aName, aDescription, isActive);
         aCategory.validate(notification);
 
-        if (notification.hasError()) {
-            // TODO Implement return
-        }
+        return notification.hasError() ? API.Left(notification) : create(aCategory);
+    }
 
-        return CreateCategoryOutput.from(this.categoryGateway.create(aCategory));
+    private Either<Notification, CreateCategoryOutput> create(final Category aCategory) {
+        return API.Try(() -> this.categoryGateway.create(aCategory))
+                .toEither()
+                // Left: Caso tenha erro / Right: Caso esteja certo
+                .bimap(Notification::create, CreateCategoryOutput::from);
     }
 }
