206279bf2c55aae6909dd6bb4cb525f71b70f545
gui-meireles <guilhermemeirelesnascimento@gmail.com>
2024-05-02 16:38:56 -0300

    Implementing Mockito annotations and more use case tests to Create Category

---
+++ application/src/main/java/com/admin/codeflix/application/category/create/CreateCategoryOutput.java
@@ -0,0 +1,13 @@
+package com.admin.codeflix.application.category.create;
+
+import com.admin.codeflix.domain.category.Category;
+import com.admin.codeflix.domain.category.CategoryID;
+
+public record CreateCategoryOutput(
+        CategoryID id
+) {
+
+    public static CreateCategoryOutput from(final Category aCategory) {
+        return new CreateCategoryOutput(aCategory.getId());
+    }
+}
---
+++ application/src/main/java/com/admin/codeflix/application/category/create/CreateCategoryUseCase.java
@@ -0,0 +1,6 @@
+package com.admin.codeflix.application.category.create;
+
+import com.admin.codeflix.application.UseCase;
+
+public abstract class CreateCategoryUseCase extends UseCase<CreateCategoryCommand, CreateCategoryOutput> {
+}
