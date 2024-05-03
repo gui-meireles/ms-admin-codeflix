ba0516cae414563ad5228586ee854e1cdbdc9176
gui-meireles <guilhermemeirelesnascimento@gmail.com>
2024-05-03 19:15:01 -0300

    Implementing Get and List Category UseCase

---
+++ application/src/main/java/com/admin/codeflix/application/category/retrieve/get/DefaultGetCategoryByIdUseCase.java
@@ -0,0 +1,33 @@
+package com.admin.codeflix.application.category.retrieve.get;
+
+import com.admin.codeflix.domain.category.CategoryGateway;
+import com.admin.codeflix.domain.category.CategoryID;
+import com.admin.codeflix.domain.exceptions.DomainException;
+import com.admin.codeflix.domain.validation.Error;
+
+import java.util.Objects;
+import java.util.function.Supplier;
+
+public class DefaultGetCategoryByIdUseCase extends GetCategoryByIdUseCase {
+
+    private final CategoryGateway categoryGateway;
+
+    public DefaultGetCategoryByIdUseCase(final CategoryGateway categoryGateway) {
+        this.categoryGateway = Objects.requireNonNull(categoryGateway);
+    }
+
+    @Override
+    public CategoryOutput execute(final String anId) {
+        final var aCategoryId = CategoryID.from(anId);
+
+        return this.categoryGateway.findById(aCategoryId)
+                .map(CategoryOutput::from)
+                .orElseThrow(notFound(aCategoryId));
+    }
+
+    private Supplier<DomainException> notFound(final CategoryID anId) {
+        return () -> DomainException.with(
+                new Error("Category with ID %s was not found".formatted(anId.getValue()))
+        );
+    }
+}
---
+++ application/src/main/java/com/admin/codeflix/application/category/retrieve/list/DefaultListCategoriesUseCase.java
@@ -0,0 +1,22 @@
+package com.admin.codeflix.application.category.retrieve.list;
+
+import com.admin.codeflix.domain.category.CategoryGateway;
+import com.admin.codeflix.domain.category.CategorySearchQuery;
+import com.admin.codeflix.domain.pagination.Pagination;
+
+import java.util.Objects;
+
+public class DefaultListCategoriesUseCase extends ListCategoriesUseCase {
+
+    private final CategoryGateway categoryGateway;
+
+    public DefaultListCategoriesUseCase(final CategoryGateway categoryGateway) {
+        this.categoryGateway = Objects.requireNonNull(categoryGateway);
+    }
+
+    @Override
+    public Pagination<CategoryListOutput> execute(final CategorySearchQuery aQuery) {
+        return this.categoryGateway.findAll(aQuery)
+                .map(CategoryListOutput::from);
+    }
+}
