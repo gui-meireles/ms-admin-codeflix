0d8294d5931b7d4b3be4b942710d2f47f9d8a609
gui-meireles <guilhermemeirelesnascimento@gmail.com>
2024-05-03 19:26:45 -0300

    Removing unnecessary imports

--- application/src/main/java/com/admin/codeflix/application/UseCase.java
+++ application/src/main/java/com/admin/codeflix/application/UseCase.java
@@ -1,7 +1,5 @@
 package com.admin.codeflix.application;
 
-import com.admin.codeflix.domain.category.Category;
-
 public abstract class UseCase<IN, OUT> {
 
     public abstract OUT execute(IN anInd);
