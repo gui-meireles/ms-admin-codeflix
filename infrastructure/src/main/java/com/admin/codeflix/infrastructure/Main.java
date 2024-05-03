46ac05965dcb1bcb68e87a17289f1b3f996a069c
gui-meireles <guilhermemeirelesnascimento@gmail.com>
2024-05-03 19:56:59 -0300

    Initiated SpringBoot infrastructure

--- infrastructure/src/main/java/com/admin/codeflix/infrastructure/Main.java
+++ infrastructure/src/main/java/com/admin/codeflix/infrastructure/Main.java
@@ -1,9 +1,14 @@
 package com.admin.codeflix.infrastructure;
 
+import com.admin.codeflix.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
-
-//        System.out.println(new UseCase().execute());
+        SpringApplication.run(WebServerConfig.class, args);
     }
 }
