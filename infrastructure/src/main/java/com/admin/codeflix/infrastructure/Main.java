284ec4ea1fc150a733a49ec86c2d569786b8f14d
gui-meireles <guilhermemeirelesnascimento@gmail.com>
2024-05-05 18:59:32 -0300

    Implementing CategoryRepository JPA

--- infrastructure/src/main/java/com/admin/codeflix/infrastructure/Main.java
+++ infrastructure/src/main/java/com/admin/codeflix/infrastructure/Main.java
@@ -1,13 +1,33 @@
 package com.admin.codeflix.infrastructure;
 
+import com.admin.codeflix.domain.category.Category;
+import com.admin.codeflix.infrastructure.category.persistence.CategoryJpaEntity;
+import com.admin.codeflix.infrastructure.category.persistence.CategoryRepository;
 import com.admin.codeflix.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(WebServerConfig.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner runner(CategoryRepository repository) {
+        return args -> {
+            List<CategoryJpaEntity> all = repository.findAll();
+
+            // Tests
+            Category filmes = Category.newCategory("Filmes", null, true);
+
+            repository.saveAndFlush(CategoryJpaEntity.from(filmes));
+            repository.deleteAll();
+        };
     }
 }
