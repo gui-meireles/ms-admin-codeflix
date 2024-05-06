e5c495726cd30cc485a808a0dba04fb3eb70b16e
gui-meireles <guilhermemeirelesnascimento@gmail.com>
2024-05-06 17:15:43 -0300

    Implementing Springdoc and Controllers

---
+++ infrastructure/src/main/java/com/admin/codeflix/infrastructure/api/controllers/CategoryController.java
@@ -0,0 +1,19 @@
+package com.admin.codeflix.infrastructure.api.controllers;
+
+import com.admin.codeflix.domain.pagination.Pagination;
+import com.admin.codeflix.infrastructure.api.CategoryAPI;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CategoryController implements CategoryAPI {
+    @Override
+    public ResponseEntity<?> createCategory() {
+        return null;
+    }
+
+    @Override
+    public Pagination<?> listCategories(String search, int page, int perPage, String sort, String direction) {
+        return null;
+    }
+}
