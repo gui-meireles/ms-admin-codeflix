e662b77be9bebf975a047a8214cef022a6ea1ae8
gui-meireles <guilhermemeirelesnascimento@gmail.com>
2024-05-06 17:24:11 -0300

    Implementing strategic to unit tests of Controller

--- infrastructure/src/main/java/com/admin/codeflix/infrastructure/api/controllers/CategoryController.java
+++ infrastructure/src/main/java/com/admin/codeflix/infrastructure/api/controllers/CategoryController.java
@@ -1,12 +1,22 @@
 package com.admin.codeflix.infrastructure.api.controllers;
 
+import com.admin.codeflix.application.category.create.CreateCategoryUseCase;
 import com.admin.codeflix.domain.pagination.Pagination;
 import com.admin.codeflix.infrastructure.api.CategoryAPI;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @RestController
 public class CategoryController implements CategoryAPI {
+
+    private final CreateCategoryUseCase createCategoryUseCase;
+
+    public CategoryController(final CreateCategoryUseCase createCategoryUseCase) {
+        this.createCategoryUseCase = Objects.requireNonNull(createCategoryUseCase);
+    }
+
     @Override
     public ResponseEntity<?> createCategory() {
         return null;
