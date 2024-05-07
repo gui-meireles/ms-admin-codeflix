23b1ebdf34a4e5fba5d173fcac1ad4d9dc4d2b86
gui-meireles <guilhermemeirelesnascimento@gmail.com>
2024-05-06 22:10:45 -0300

    implementing integrated testing of the category creation

--- infrastructure/src/test/java/com/admin/codeflix/infrastructure/api/CategoryAPITest.java
+++ infrastructure/src/test/java/com/admin/codeflix/infrastructure/api/CategoryAPITest.java
@@ -1,11 +1,24 @@
 package com.admin.codeflix.infrastructure.api;
 
 import com.admin.codeflix.ControllerTest;
+import com.admin.codeflix.application.category.create.CreateCategoryOutput;
 import com.admin.codeflix.application.category.create.CreateCategoryUseCase;
+import com.admin.codeflix.domain.category.CategoryID;
+import com.admin.codeflix.infrastructure.category.models.CreateCategoryApiInput;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.vavr.API;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.Objects;
+
+import static org.mockito.Mockito.*;
 
 @ControllerTest(controllers = CategoryAPI.class)
 public class CategoryAPITest {
@@ -13,11 +26,38 @@ public class CategoryAPITest {
     @Autowired
     private MockMvc mvc;
 
+    @Autowired
+    private ObjectMapper mapper;
+
     @MockBean
     private CreateCategoryUseCase createCategoryUseCase;
 
     @Test
-    public void test() {
+    public void givenAValidCommand_whenCallsCreateCategory_shouldReturnCategoryId() throws Exception {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
 
+        final var aInput = new CreateCategoryApiInput(expectedName, expectedDescription, expectedIsActive);
+
+        when(createCategoryUseCase.execute(any()))
+                .thenReturn(API.Right(CreateCategoryOutput.from(CategoryID.from("123"))));
+
+        final var request = MockMvcRequestBuilders.post("/categories")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(aInput));
+
+        this.mvc.perform(request)
+                .andDo(MockMvcResultHandlers.print())
+                .andExpectAll(
+                        MockMvcResultMatchers.status().isCreated(),
+                        MockMvcResultMatchers.header().string("location", "/categories/123")
+                );
+
+        verify(createCategoryUseCase, times(1)).execute(argThat(cmd ->
+                Objects.equals(expectedName, cmd.name())
+                        && Objects.equals(expectedDescription, cmd.description())
+                        && Objects.equals(expectedIsActive, cmd.isActive())
+        ));
     }
 }
